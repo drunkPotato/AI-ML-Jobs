863b87bdcec3c268cb431dedf6399f004da52349
Corsin <corsinxhidber@gmail.com>
2025-02-15 08:44:13 +0100

    Removed unnecessary files

--- myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import home, plot, apply_filters
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('plot/', plot, name='plot'),
-    path('apply-filters/', apply_filters, name='apply_filters'),
-]
--- src/my_script/Data/formatting/processlistings.py
+++
@@ -1,4 +0,0 @@
-import re
-
-def process(list):
-    pass
